21022ff0ab84440419323b8704b484a6258e5588
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2021-08-25 19:04:13 -0300

    Adding sandbox to project

---
+++ sandbox/recorder.js
@@ -0,0 +1,25 @@
+$recorder = null
+
+window.onload = async function () {
+  $recorder = await Recorder()
+}
+
+document.getElementById('rec').addEventListener('click', async () => {
+  document.getElementById('feedback').style.display = 'flex'
+  $recorder.start()
+})
+
+document.getElementById('stop').addEventListener('click', async () => {
+  $recorder.stop()
+  document.getElementById('feedback').style.display = 'none'
+
+  const media = await $recorder.getMedia()
+
+  document.getElementById('result').src = media.audioUrl
+})
+
+document.getElementById('pause').addEventListener('click', async () => {
+  document.getElementById('feedback').style.display = 'none'
+
+  $recorder.pause()
+})
